3edd278063b439b9717cdb106deb704983c628ce
rubenperegrina <peregrinaruben@gmail.com>
2022-07-26 16:56:16 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# js-4jaxnp
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-4jaxnp)
---
+++ index.js
@@ -0,0 +1,16 @@
+
+import './style.css';
+
+const h2s = document.querySelectorAll('h2')
+
+const observer = new IntersectionObserver((entries) => {
+	for (const entry of entries) {
+  	if (entry.isIntersecting) {
+	    entry.target.classList.add('visible')
+    } else {
+			entry.target.classList.remove('visible')
+    }
+  }
+}, { threshold: 1.0, rootMargin: '20%' })
+  
+h2s.forEach(el => observer.observe(el))
